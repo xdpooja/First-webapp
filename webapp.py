09a488bcb73db593a115c8fabb8d1c397790e57c
xdpooja <justpooja16@gmail.com>
2023-03-05 19:09:57 +0530

    new commit

---
+++ webapp.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.set_page_config(page_title = "My webpage", page_icon=":tada:", layout="wide")
+
+st.subheader("Hi, I'm Pooja")
+st.title("This is my first webapp")
+st.write("testing")
